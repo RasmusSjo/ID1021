89d9eb9fcf03027b6418fb299c04ab41f9b94ace
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-09-21 13:58:38 +0200

    Added benchmark for assignment 4 and changed result from single array to double

--- src/utils/Benchmarker.java
+++ src/utils/Benchmarker.java
@@ -1,5 +1,7 @@
 package utils;
 
+import single_linked_list.ArrayList;
+import single_linked_list.LinkedList;
 import sorted_data.Search;
 import sorting.ArraySorter;
 
@@ -8,6 +10,8 @@ import java.util.Arrays;
 public class Benchmarker {
 
     static int[] sizes;
+    static int numOfInput;
+
     static int iterations;
     static String[] rowNames;
     static String[] columnNames;
@@ -20,6 +24,7 @@ public class Benchmarker {
             }
             case 2: {
                 sizes = new int[]{100, 200, 400, 800, 2000, 4000, 8000, 16000, 32000, 64000};
+                numOfInput = 1;
                 iterations = 1000;
                 rowNames = LatexHelper.intArrayToStrArray(sizes);
                 columnNames = new String[]{"Array size", "Linear search (unsorted)", "Linear search (sorted)", "Binary search"};
@@ -27,19 +32,30 @@ public class Benchmarker {
             }
             case 3: {
                 sizes = new int[]{100, 300, 500, 700, 900};
+                numOfInput = 1;
                 iterations = 1000;
                 rowNames = LatexHelper.intArrayToStrArray(sizes);
                 columnNames = new String[]{"Array size", "Selection sort", "Insertion sort", "Merge sort", "Quick sort"};
                 break;
             }
+            case 4: {
+                sizes = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000};
+                numOfInput = 2;
+                iterations = 1000;
+                rowNames = LatexHelper.intArrayToStrArray(sizes);
+                columnNames = new String[]{"List1", "List2", "Runtime (us)", "List1", "List2", "Runtime (us)",
+                        "List1", "List2", "Runtime (us)", "List1", "List2", "Runtime (us)"};
+                break;
+            }
         }
     }
 
-    public static double[] bench(int assignmentNumber) {
+    public static double[][] bench(int assignmentNumber) {
 
         return switch (assignmentNumber) {
-            case 2 -> sortedData();
+            //case 2 -> sortedData();
             case 3 -> sorting();
+            case 4 -> singleLinked();
             default -> throw new IllegalStateException("Unexpected value: " + assignmentNumber);
         };
     }
@@ -109,11 +125,12 @@ public class Benchmarker {
         return benchmark;
     }
 
-    public static double[] sorting() {
-        int rowLength = columnNames.length - 1;
+    public static double[][] sorting() {
+        int rowLength = columnNames.length;
+        int columnLength = sizes.length;
 
         // Array to store the benchmark data
-        double[] benchmark = new double[sizes.length * rowLength];
+        double[][] benchmark = new double[columnLength][rowLength];
 
         long t1, t2, t3, t4;
         long time1, time2, time3, time4;
@@ -155,26 +172,110 @@ public class Benchmarker {
                 minTime4 = minTime4 > time4 ? time4 : minTime4;
             }
 
-            benchmark[rowLength * sizeNum] = minTime1;
-            benchmark[rowLength * sizeNum + 1] = minTime2;
-            benchmark[rowLength * sizeNum + 2] = minTime3;
-            benchmark[rowLength * sizeNum + 3] = minTime4;
+            benchmark[sizeNum][0] = sizes[sizeNum];
+            benchmark[sizeNum][1] = minTime1;
+            benchmark[sizeNum][2] = minTime2;
+            benchmark[sizeNum][3] = minTime3;
+            benchmark[sizeNum][4] = minTime4;
+        }
+
+        return benchmark;
+    }
+
+    private static double[][] singleLinked() {
+        int rowLength = columnNames.length;
+        int columnLength = sizes.length;
+
+        int fixedSize = 10 * sizes[0];
+
+        // Array to store the benchmark data
+        double[][] benchmark = new double[columnLength][rowLength];
+
+        long t1;
+        long time1;
+        double minTime1;
+
+        for (int size = 0; size < columnLength; size++) {
+            minTime1 = Double.POSITIVE_INFINITY;
+            LinkedList fixedLinked = new LinkedList(fixedSize);
+            LinkedList dynamicLinked = new LinkedList(sizes[size]);
+            ArrayList fixedArray = new ArrayList(fixedSize);
+            ArrayList dynamicArray = new ArrayList(sizes[size]);
+
+            // Append a LinkedList with fixed size to one with an increasing size of sizes[i]
+            for (int i = 0; i < iterations; i++) {
+                dynamicLinked = new LinkedList(sizes[size]);
+
+                t1 = System.nanoTime();
+                dynamicLinked.append(fixedLinked);
+                time1 = System.nanoTime() - t1;
+                minTime1 = minTime1 > time1 ? time1 : minTime1;
+            }
+
+            benchmark[size][0] = sizes[size];
+            benchmark[size][1] = fixedSize;
+            benchmark[size][2] = minTime1;
+
+            minTime1 = Double.POSITIVE_INFINITY;
+            // Append a LinkedList with dynamic size to one with a fixed size of 10 * sizes[0]
+            for (int i = 0; i < iterations; i++) {
+                fixedLinked = new LinkedList(fixedSize);
+
+                t1 = System.nanoTime();
+                fixedLinked.append(dynamicLinked);
+                time1 = System.nanoTime() - t1;
+                minTime1 = minTime1 > time1 ? time1 : minTime1;
+            }
+
+            benchmark[size][3] = fixedSize;
+            benchmark[size][4] = sizes[size];
+            benchmark[size][5] = minTime1;
+
+            minTime1 = Double.POSITIVE_INFINITY;
+            // Append an ArrayList with fixed size to one with an increasing size of sizes[i]
+            for (int i = 0; i < iterations; i++) {
+                dynamicArray = new ArrayList(sizes[size]);
+
+                t1 = System.nanoTime();
+                dynamicArray.append(fixedArray);
+                time1 = System.nanoTime() - t1;
+                minTime1 = minTime1 > time1 ? time1 : minTime1;
+            }
+
+            benchmark[size][6] = sizes[size];
+            benchmark[size][7] = fixedSize;
+            benchmark[size][8] = minTime1;
+
+            minTime1 = Double.POSITIVE_INFINITY;
+            // Append an ArrayList with increasing size to one with a fixed size of sizes[i]
+            for (int i = 0; i < iterations; i++) {
+                fixedArray = new ArrayList(fixedSize);
+
+                t1 = System.nanoTime();
+                fixedArray.append(dynamicArray);
+                time1 = System.nanoTime() - t1;
+                minTime1 = minTime1 > time1 ? time1 : minTime1;
+            }
+
+            benchmark[size][9] = fixedSize;
+            benchmark[size][10] = sizes[size];
+            benchmark[size][11] = minTime1;
         }
 
         return benchmark;
     }
 
     public static void main(String[] args) {
-        int assignmentNumber = 2;
+        int assignmentNumber = 4;
 
         setupBench(assignmentNumber);
 
-        double[] result = bench(assignmentNumber);
+        double[][] result = bench(assignmentNumber);
 
-        String tableData = LatexHelper.createTable(result, rowNames, columnNames);
-        String graphData = LatexHelper.createGraph(result, sizes, columnNames.length - 1);
+        String tableData = LatexHelper.createTable(result, columnNames, numOfInput);
+        String graphData = LatexHelper.createGraph(result, numOfInput);
 
-        LatexHelper.write(tableData, "search_table.txt");
-        LatexHelper.write(graphData, "search_graph.dat");
+        LatexHelper.write(tableData, "linked_table.txt");
+        LatexHelper.write(graphData, "linked_graph.dat");
     }
 }
