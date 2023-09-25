fee157b82cb074f9ef3256afa4602f42448acb96
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-09-25 13:42:32 +0200

    Benchmark for doubly linked list and cleanup

--- src/utils/Benchmarker.java
+++ src/utils/Benchmarker.java
@@ -1,7 +1,8 @@
 package utils;
 
+import double_linked_list.*;
 import single_linked_list.ArrayList;
-import single_linked_list.LinkedList;
+import single_linked_list.*;
 import sorted_data.Search;
 import sorting.ArraySorter;
 
@@ -9,43 +10,54 @@ import java.util.Arrays;
 
 public class Benchmarker {
 
-    static int[] sizes;
-    static int numOfInput;
+    // Variables related to the file
+    private static String fileName;
+    private final static boolean type = true; // table or graph
 
-    static int iterations;
+    // Variables related to the format of the table/graph
     static String[] rowNames;
     static String[] columnNames;
+    static int[] rowsToIgnore;
+
+    // Variables related to the benchmark
+    static int[] sizes;
+    static int iterations;
 
     private static void setupBench(int assignmentNumber) {
 
         switch (assignmentNumber) {
-            case 1: {
-
-            }
-            case 2: {
-                sizes = new int[]{100, 200, 400, 800, 2000, 4000, 8000, 16000, 32000, 64000};
-                numOfInput = 1;
-                iterations = 1000;
-                rowNames = LatexHelper.intArrayToStrArray(sizes);
+            case 2 -> {
+                fileName = "search";
                 columnNames = new String[]{"Array size", "Linear search (unsorted)", "Linear search (sorted)", "Binary search"};
-                break;
-            }
-            case 3: {
-                sizes = new int[]{100, 300, 500, 700, 900};
-                numOfInput = 1;
+                rowsToIgnore = new int[]{0};
+
+                sizes = new int[]{100, 200, 400, 800, 2000, 4000, 8000, 16000, 32000, 64000};
                 iterations = 1000;
-                rowNames = LatexHelper.intArrayToStrArray(sizes);
+            }
+            case 3 -> {
+                fileName = "sorting";
                 columnNames = new String[]{"Array size", "Selection sort", "Insertion sort", "Merge sort", "Quick sort"};
-                break;
-            }
-            case 4: {
-                sizes = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000};
-                numOfInput = 2;
+                rowsToIgnore = new int[]{0};
+
+                sizes = new int[]{100, 300, 500, 700, 900};
                 iterations = 1000;
-                rowNames = LatexHelper.intArrayToStrArray(sizes);
+            }
+            case 4 -> {
+                fileName = "single";
                 columnNames = new String[]{"List1", "List2", "Runtime (us)", "List1", "List2", "Runtime (us)",
                         "List1", "List2", "Runtime (us)", "List1", "List2", "Runtime (us)"};
-                break;
+                rowsToIgnore = new int[]{0,1,3,4,6,7,9,10};
+
+                sizes = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000};
+                iterations = 1000;
+            }
+            case 5 -> {
+                fileName = "doubly";
+                columnNames = new String[]{"List size", "Doubly (us)", "Linked (us)", "Ratio"};
+                rowsToIgnore = new int[]{0};
+
+                sizes = new int[]{10, 20, 40, 100, 200, 400, 1000, 2000, 4000, 8000, 16000, 32000, 64000};
+                iterations = 1000;
             }
         }
     }
@@ -53,29 +65,32 @@ public class Benchmarker {
     public static double[][] bench(int assignmentNumber) {
 
         return switch (assignmentNumber) {
-            //case 2 -> sortedData();
+            case 2 -> sortedData();
             case 3 -> sorting();
             case 4 -> singleLinked();
+            case 5 -> doublyLinked();
+            case 6 -> binaryTree();
             default -> throw new IllegalStateException("Unexpected value: " + assignmentNumber);
         };
     }
 
-    private static double[] sortedData() {
-        int rowLength = columnNames.length - 1;
+    private static double[][] sortedData() {
+        int rowLength = columnNames.length;
+        int columnLength = sizes.length;
 
-        double[] benchmark = new double[sizes.length * rowLength];
+        double[][] benchmark = new double[columnLength][rowLength];
 
         long t1, t2, t3;
         long time1, time2, time3;
         double minTime1, minTime2, minTime3;
 
-        for (int sizeNum = 0; sizeNum < sizes.length; sizeNum++) {
-            int size = sizes[sizeNum];
+        for (int size = 0; size < columnLength; size++) {
+            int arraySize = sizes[size];
 
             int numOfKeys = 100;
+            int[] keys = ArrayGenerator.unsorted(numOfKeys, arraySize);
 
-            int[] keys = ArrayGenerator.unsorted(numOfKeys, size);
-            int[] unsortedArray = ArrayGenerator.unsorted(size);
+            int[] unsortedArray = ArrayGenerator.unsorted(arraySize);
             int[] sortedArray = Arrays.copyOf(unsortedArray, unsortedArray.length);
             ArraySorter.mergeSort(sortedArray);
 
@@ -89,7 +104,7 @@ public class Benchmarker {
             // The arrays are switched every 10 iterations to randomize things a bit.
             for (int i = 0; i < iterations; i++) {
                 if (i % 10 == 0) {
-                    unsortedArray = ArrayGenerator.unsorted(size);
+                    unsortedArray = ArrayGenerator.unsorted(arraySize);
                     sortedArray = Arrays.copyOf(unsortedArray, unsortedArray.length);
                     ArraySorter.mergeSort(sortedArray);
                 }
@@ -117,9 +132,10 @@ public class Benchmarker {
             }
 
             // The average time to search for one key
-            benchmark[rowLength * sizeNum] = minTime1 / numOfKeys;
-            benchmark[rowLength * sizeNum + 1] = minTime2 / numOfKeys;
-            benchmark[rowLength * sizeNum + 2] = minTime3 / numOfKeys;
+            benchmark[size][0] = sizes[size];
+            benchmark[size][1] = minTime1 / numOfKeys;
+            benchmark[size][2] = minTime2 / numOfKeys;
+            benchmark[size][3] = minTime3 / numOfKeys;
         }
 
         return benchmark;
@@ -136,7 +152,7 @@ public class Benchmarker {
         long time1, time2, time3, time4;
         double minTime1, minTime2, minTime3, minTime4;
 
-        for (int sizeNum = 0; sizeNum < sizes.length; sizeNum++) {
+        for (int size = 0; size < columnLength; size++) {
             int[] array1;
 
             minTime1 = Double.POSITIVE_INFINITY;
@@ -145,7 +161,7 @@ public class Benchmarker {
             minTime4 = Double.POSITIVE_INFINITY;
 
             for (int i = 0; i < iterations; i++) {
-                array1 = ArrayGenerator.unsorted(sizes[sizeNum]);
+                array1 = ArrayGenerator.unsorted(sizes[size]);
                 ArraySorter.quickSort(array1, 0, array1.length - 1);
                 int[] array2 = Arrays.copyOf(array1, array1.length);
                 int[] array3 = Arrays.copyOf(array1, array1.length);
@@ -172,11 +188,11 @@ public class Benchmarker {
                 minTime4 = minTime4 > time4 ? time4 : minTime4;
             }
 
-            benchmark[sizeNum][0] = sizes[sizeNum];
-            benchmark[sizeNum][1] = minTime1;
-            benchmark[sizeNum][2] = minTime2;
-            benchmark[sizeNum][3] = minTime3;
-            benchmark[sizeNum][4] = minTime4;
+            benchmark[size][0] = sizes[size];
+            benchmark[size][1] = minTime1;
+            benchmark[size][2] = minTime2;
+            benchmark[size][3] = minTime3;
+            benchmark[size][4] = minTime4;
         }
 
         return benchmark;
@@ -265,17 +281,90 @@ public class Benchmarker {
         return benchmark;
     }
 
+    private static double[][] doublyLinked() {
+        int rowLength = columnNames.length;
+        int columnLength = sizes.length;
+
+        // Array to store the benchmark data
+        double[][] benchmark = new double[columnLength][rowLength];
+
+        long t1;
+        long time1;
+
+        int average = 100;
+
+        int k = 1000;
+
+        for (int size = 0; size < columnLength; size++) {
+
+            double minTime1 = Double.POSITIVE_INFINITY;
+            double minTime2 = Double.POSITIVE_INFINITY;
+
+            for (int i = 0; i < average; i++) {
+
+                LinkedList singleLinked = new LinkedList(sizes[size]);
+                DoublyLinkedList doublyLinked = new DoublyLinkedList(sizes[size]);
+
+                LinkedList.Cell[] singleCellArray = singleLinked.getCellArray();
+                DoublyLinkedList.Cell[] doublyCellArray = doublyLinked.getCellArray();
+
+                int[] keys = ArrayGenerator.unsortedDup(k, sizes[size]);
+
+                // Unlink and insert a cell in a doubly linked list
+                t1 = System.nanoTime();
+                for (int key : keys) {
+                    DoublyLinkedList.Cell cell = doublyCellArray[key];
+                    doublyLinked.unlink(cell);
+                    doublyLinked.insert(cell);
+                }
+                time1 = System.nanoTime() - t1;
+                minTime1 = minTime1 > time1 ? time1 : minTime1;
+
+                // Unlink and insert a cell in a single linked list
+                t1 = System.nanoTime();
+                for (int key : keys) {
+                    LinkedList.Cell cell = singleCellArray[key];
+                    singleLinked.unlink(cell);
+                    singleLinked.insert(cell);
+                }
+                time1 = System.nanoTime() - t1;
+                minTime2 = minTime2 > time1 ? time1 : minTime2;
+            }
+
+            benchmark[size][0] = sizes[size];
+            benchmark[size][1] = minTime1;
+            benchmark[size][2] = minTime2;
+            benchmark[size][3] = minTime2 / minTime1;
+        }
+        return benchmark;
+    }
+
+    private static double[][] binaryTree() {
+        int rowLength = columnNames.length;
+        int columnLength = sizes.length;
+
+        // Array to store the benchmark data
+        double[][] benchmark = new double[columnLength][rowLength];
+
+
+
+
+
+        return benchmark;
+    }
+
+
     public static void main(String[] args) {
-        int assignmentNumber = 4;
+        int assignmentNumber = 3;
 
         setupBench(assignmentNumber);
 
         double[][] result = bench(assignmentNumber);
 
-        String tableData = LatexHelper.createTable(result, columnNames, numOfInput);
-        String graphData = LatexHelper.createGraph(result, numOfInput);
+        String tableData = LatexHelper.createTable(result, columnNames, rowsToIgnore);
+        String graphData = LatexHelper.createGraph(result, rowsToIgnore);
 
-        LatexHelper.write(tableData, "linked_table.txt");
-        LatexHelper.write(graphData, "linked_graph.dat");
+        LatexHelper.write(tableData, fileName, type);
+        LatexHelper.write(graphData, fileName, !type);
     }
 }
