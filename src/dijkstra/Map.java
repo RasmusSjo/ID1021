57806a0b397e462f9a2772a6d3daffef9b6791db
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-10-28 02:57:12 +0200

    New cities are now given an ID when added to the map

---
+++ src/dijkstra/Map.java
@@ -0,0 +1,117 @@
+package dijkstra;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Map {
+
+    private final int MODULO = 541;
+    private City[] cities;
+    private int numOfCities;
+
+    public Map(String fileName) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            cities = new City[MODULO];
+            numOfCities = 0;
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] information = line.split(",");
+
+                String fromName = information[0];
+                String toName = information[1];
+                int minutes = Integer.parseInt(information[2]);
+
+                // Will add the cities to the array of cities
+                City from = lookup(fromName);
+                City to = lookup(toName);
+
+                // Add connection to both cities
+                from.addConnection(to, minutes);
+                to.addConnection(from, minutes);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(" file " + fileName + " not found");
+            e.printStackTrace();
+        }
+    }
+
+    public void addCity(City city) {
+        int hashIndex = getHashCode(city.name);
+
+        // If there is a collision, go to the next index
+        while (collision(hashIndex)) {
+            hashIndex++;
+        }
+
+        numOfCities++;
+        cities[hashIndex] = city;
+    }
+
+    public City getCity(String name) {
+        int hashIndex = getHashCode(name);
+
+        // If the city being looked at is null, the city hasn't been added to the array of cities
+        while (cities[hashIndex] != null && !cities[hashIndex].name.equals(name)) {
+            hashIndex++;
+            // Keep the hash index in bounds
+            if (hashIndex >= MODULO) {
+                hashIndex = 0;
+            }
+        }
+
+        return cities[hashIndex];
+    }
+
+    public ArrayList<City> getCities(String name) {
+        City from = getCity(name);
+        ArrayList<City> cities = new ArrayList<>();
+        cities.add(from);
+
+        for (int i = 0; i < cities.size(); i++) {
+            City city = cities.get(i);
+            for (int j = 0; j < city.neighbours.length; j++) {
+                Connection neighbour = city.neighbours[j];
+                if (!cities.contains(neighbour.city)) {
+                    cities.add(neighbour.city);
+                }
+            }
+        }
+
+        // Remove initial city from list
+        cities.remove(0);
+        return cities;
+    }
+
+    public int getNumOfCities() {
+        return numOfCities;
+    }
+
+    private City lookup(String name) {
+        City city = getCity(name);
+
+        if (city == null) {
+            city = new City(name, numOfCities++);
+
+            addCity(city);
+        }
+
+        return city;
+    }
+
+    private int getHashCode(String name) {
+        int hash = 0;
+        for (int i = 0; i < name.length(); i++) {
+            hash = (hash * 31 % MODULO) + name.charAt(i);
+        }
+        return hash % MODULO;
+    }
+
+    private boolean collision(int index) {
+        return cities[index] != null;
+    }
+}
