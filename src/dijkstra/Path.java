9e33b2de095918094521dd56010596110b7b9a96
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-10-28 02:55:06 +0200

    Removed one of the constructors for the path object

--- src/dijkstra/Path.java
+++ src/dijkstra/Path.java
@@ -1,7 +1,5 @@
 package dijkstra;
 
-import graph.City;
-
 public class Path {
 
     public City city;
@@ -9,12 +7,8 @@ public class Path {
     public Integer distance;
     public Integer index;
 
-    public Path(City city, Integer index) {
-        this(city, null, Integer.MAX_VALUE);
-    }
-
     public Path(City city, City prev, Integer distance) {
-        this(city, prev, distance, 0);
+        this(city, prev, distance, null);
     }
 
     public Path(City city, City prev, Integer distance, Integer index) {
